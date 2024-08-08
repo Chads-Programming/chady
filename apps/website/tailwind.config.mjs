52b2a8bfdbe3ba65518d1802a03c833ab0ccb2f2
Andres Velasco <velasco.andrs@gmail.com>
2024-08-07 21:58:15 -0500

    chore: setup monorepo

---
+++ apps/website/tailwind.config.mjs
@@ -0,0 +1,11 @@
+import sharedConfig from "@repo/tailwind-config";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  presets: [sharedConfig],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
