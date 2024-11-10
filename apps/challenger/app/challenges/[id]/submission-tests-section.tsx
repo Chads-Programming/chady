0f566a55fd5574d49e52b8102edf314bb76ed544
Andres Velasco <velasco.andrs@gmail.com>
2024-11-09 20:54:16 -0500

    feat(submissions): refactor test results component

--- apps/challenger/app/challenges/[id]/submission-tests-section.tsx
+++ apps/challenger/app/challenges/[id]/submission-tests-section.tsx
@@ -1,10 +1,6 @@
 import type { InputExecutionResult } from '@/graphql/graphql'
 import { SubmissionResultCard } from '../components/submission-result-card'
-import {
-  SecretTestResult,
-  TestCases,
-  TestResult,
-} from '../components/test-summary'
+import { TestResults, type TestSummaryProps } from '../components/test-results'
 
 interface Props {
   score: number
@@ -19,6 +15,16 @@ export const SubmissionTestsSection = ({
 }: Props) => {
   const totalTests = testResults.length
   const totalSuccess = testResults.filter(({ isSuccess }) => isSuccess).length
+  const tests: TestSummaryProps['testResults'] = testResults.map(
+    ({ testCase, isSuccess }) => ({
+      id: testCase.id.toString(),
+      input: testCase.args,
+      expectedOutput: testCase.expectedOutput,
+      currentOuput: testCase.expectedOutput,
+      isSecret: testCase.isSecret,
+      isSuccess,
+    }),
+  )
 
   return (
     <div className="grid grid-cols-2 w-full">
@@ -29,26 +35,7 @@ export const SubmissionTestsSection = ({
         totalSuccess={totalSuccess}
       />
 
-      <TestCases>
-        {testResults.map(({ isSuccess, testCase, output }) =>
-          testCase.isSecret ? (
-            <SecretTestResult
-              key={testCase.id}
-              id={testCase.id.toString()}
-              isSuccess={isSuccess}
-            />
-          ) : (
-            <TestResult
-              key={testCase.id}
-              id={testCase.id.toString()}
-              isSuccess={isSuccess}
-              input={testCase.args.toString()}
-              currentOuput={output}
-              expectedOutput={testCase.expectedOutput}
-            />
-          ),
-        )}
-      </TestCases>
+      <TestResults testResults={tests} />
     </div>
   )
 }
